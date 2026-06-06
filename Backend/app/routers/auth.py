640f5655a7c7d9442a6cf02f6417c7aa84582d0e
Angel Singh Shrestha <138711417+AngelShresthaCS@users.noreply.github.com>
2026-06-06 01:06:01 -0500

    OAUTH2 Added for authentication

--- Backend/app/routers/auth.py
+++ Backend/app/routers/auth.py
@@ -10,20 +10,20 @@ router = APIRouter(
 )
 
 @router.post("/")
-def login(user_credentials: schemas.UserLogin, db: Session = Depends(database.get_db)):
-    user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
+    user = db.query(models.User).filter(models.User.username == user_credentials.username).first()
     if not user:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, 
+            status_code=status.HTTP_401_UNAUTHORIZED, 
             detail="Invalid Credentials"
         )
     if not hashing.verify(user_credentials.password, user.password):
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, 
+            status_code=status.HTTP_401_UNAUTHORIZED, 
             detail="Invalid Credentials"
         )
     # Create a token and return it
-    access_token = oauth2.create_access_token(data={"username":user.username})
+    access_token = oauth2.create_access_token(data={"user_id":user.id})
 
     return {"access_token":access_token, "token_type":"bearer"}
 
